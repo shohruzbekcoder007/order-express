b259a4bb1f0c3939cff1aa76cade7997009aa379
shohruzbekcoder007 <shohruzbekcoder007@gmail.com>
2025-01-17 11:13:05 +0500

    vercel json9

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ app.use((req, res, next) => {
     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
     next();
 });
-app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cors("*"))
 app.use(bodyParser.json())
 app.use('/user', user)
