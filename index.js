acd4b6e439e898087642052de92cd5747cad48c8
shohruzbekcoder007 <shohruzbekcoder007@gmail.com>
2025-01-17 09:52:39 +0500

    vercel json4

--- index.js
+++ index.js
@@ -6,6 +6,11 @@ const foodtype = require('./routers/foodtype.router')
 const restaurant = require('./routers/restaurant.router')
 const app = express()
 
+app.use((req, res, next) => {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+});
 app.use(cors())
 app.use(bodyParser.json())
 app.use('/user', user)
