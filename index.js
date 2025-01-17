8ae356230adfbc4a7e5c75aaabbe597a4aba832b
shohruzbekcoder007 <shohruzbekcoder007@gmail.com>
2025-01-17 12:23:31 +0500

    add swagger

--- index.js
+++ index.js
@@ -1,25 +1,39 @@
-const express = require("express")
-const bodyParser = require("body-parser")
-const cors = require("cors")
-const user = require('./routers/user.router')
-const foodtype = require('./routers/foodtype.router')
-const restaurant = require('./routers/restaurant.router')
-const app = express()
+const express = require("express");
+const bodyParser = require("body-parser");
+const cors = require("cors");
+const user = require('./routers/user.router');
+const foodtype = require('./routers/foodtype.router');
+const restaurant = require('./routers/restaurant.router');
 
-app.use((req, res, next) => {
-    res.header("Access-Control-Allow-Origin", "*");
-    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-    next();
-});
+const swaggerJsdoc = require("swagger-jsdoc")
+const swaggerUi = require("swagger-ui-express")
+const options = require("./swagger.json")
+
+const app = express();
+
+const specs = swaggerJsdoc({
+    swaggerDefinition: options,
+    apis: ["./routers/*.js"]
+})
+
+app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
-app.use(cors())
-app.use(bodyParser.json())
-app.use('/user', user)
-app.use('/foodtype', foodtype)
-app.use('/restaurant', restaurant)
+app.use(cors());
+
+app.use('/api/user', user);
+app.use('/api/foodtype', foodtype);
+app.use('/api/restaurant', restaurant);
+
+app.use(
+    "/api-docs",
+    swaggerUi.serve,
+    swaggerUi.setup(specs
+        // ,{ explorer: true }
+    )
+);
 
 // Serverni ishga tushirish
-const PORT = process.env.PORT || 8085
-app.listen(PORT, () => console.log(`Server running on port ${PORT}`))
+const PORT = process.env.PORT || 8085;
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
 
 
